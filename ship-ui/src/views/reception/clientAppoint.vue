fedb436c87cedccc45768699abbae750eae65e23
MixJade <1114185977@qq.com>
2023-07-12 15:31:05 +0800

    个人中心的路由样式抽离

--- ship-ui/src/views/reception/clientAppoint.vue
+++ ship-ui/src/views/reception/clientAppoint.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="base-page">
-    <el-card shadow="always" v-for="a in appointList" :key="a.appointmentId">
-      <template #header>
-        <div>
-          <span>单号：{{ a.appointmentId }}</span>
-        </div>
-      </template>
-      <ul style="list-style: none">
-        <li><span>就诊时间：</span>{{ moveT(a.appointmentDate) }}</li>
-        <li><span>就诊宠物：</span>{{ a.petName }}</li>
-        <li><span>科室：</span>{{ a.departmentName }}&nbsp;<span>医生：</span>{{ a.doctorName }}</li>
-        <li><span>描述：</span>{{ a.appointmentInfo }}</li>
-        <li><span>挂号时间:{{ moveT(a.createTime) }}</span></li>
-      </ul>
-    </el-card>
-  </div>
-
+  <el-card shadow="always" v-for="a in appointList" :key="a.appointmentId">
+    <template #header>
+      <div>
+        <span>单号：{{ a.appointmentId }}</span>
+      </div>
+    </template>
+    <ul style="list-style: none">
+      <li><span>就诊时间：</span>{{ moveT(a.appointmentDate) }}</li>
+      <li><span>就诊宠物：</span>{{ a.petName }}</li>
+      <li><span>科室：</span>{{ a.departmentName }}&nbsp;<span>医生：</span>{{ a.doctorName }}</li>
+      <li><span>描述：</span>{{ a.appointmentInfo }}</li>
+      <li><span>挂号时间:{{ moveT(a.createTime) }}</span></li>
+    </ul>
+  </el-card>
 </template>
 
 <script setup lang="ts">
@@ -26,18 +23,11 @@ const appointList = exampleClientAppoint()
 </script>
 
 <style scoped lang="scss">
-.base-page {
-  position: fixed;
-  width: 50vw;
-  left: 25vw;
-  top: 15vh;
+.el-card {
+  margin-left: 5px;
 
-  .el-card {
-    margin-left: 5px;
-
-    span {
-      font-size: small;
-    }
+  span {
+    font-size: small;
   }
 }
 </style>
