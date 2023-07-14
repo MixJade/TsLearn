591c1fcf19593a2474b84111233aaa0d60221490
MixJade <1114185977@qq.com>
2023-07-14 17:54:03 +0800

    后台管理页面的操作栏

--- ship-ui/src/views/backstage/y-petList.vue
+++ ship-ui/src/views/backstage/y-petList.vue
@@ -1,8 +1,27 @@
 <template>
-<h3>宠物</h3>
+  <BackOpCol @query="queryB" @addRole="addRoleB" @delBatch="delBatchB">
+    <el-input v-model="qp.petName" placeholder="宠物姓名" size="large"/>
+    <el-input v-model="qp.clientName" placeholder="用户姓名" size="large"/>
+  </BackOpCol>
 </template>
 
 <script setup lang="ts">
+import {reactive} from 'vue'
+import BackOpCol from "@/components/BackOpCol.vue";
+
+const qp = reactive({
+  petName: '',
+  clientName: ''
+})
+const queryB = (): void => {
+  console.log("查询，参数1：", qp.petName, "参数2", qp.clientName)
+}
+const addRoleB = (): void => {
+  console.log("添加宠物")
+}
+const delBatchB = (): void => {
+  console.log("批量删除")
+}
 </script>
 
 <style scoped>
