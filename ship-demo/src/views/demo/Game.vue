0da2d1a43a3a3c2c014a77fd6f9f89f4456a9532
MixJade <1114185977@qq.com>
2023-08-05 11:37:57 +0800

    游戏DEMO完善,404路由的配置

--- ship-demo/src/views/demo/Game.vue
+++ ship-demo/src/views/demo/Game.vue
@@ -1,37 +1,55 @@
 <template>
-
-  <button @click="stop">停止</button>
-  <button @click="start">游戏开始</button>
-  <div style="color: red; text-align: center;font-size: 25px">score:{{scroce}}</div>
-
-  <div class="box" :style="{width :boxWidth +'px', height:boxHeight +'px'}">
-    <div class="str">{{str}}</div>
-    <div class="kuaiBox">
-      <div class="kuai" v-for="(item,index) in arr" :key="index" :style="{opacity :item.active ? '0':'1'}"></div>
+  <div class="panel"
+       @click="focusIn"
+       @keyup.space="start"
+       @keyup.esc="stop"
+       @keydown.left="moveLeft"
+       @keydown.right="moveRight"
+  >
+    <input ref="myInput" class="myInput">
+    <h1>{{ state.score }}</h1>
+    <p>你来到了网站的尽头，这里只有一个小游戏</p>
+    <p>按空格开始游戏，按ESC暂停</p>
+    <p>按下左右方向键移动挡板</p>
+    <!--游戏面板-->
+    <div class="box">
+      <div class="str">{{ state.str }}</div>
+      <div class="brickBox">
+        <div v-for="(item,index) in state.arr" :key="index" :ref="el=>(brickRef[index]=el)"
+             :style="{opacity :item.active ? '0':'1'}"
+             class="brick"></div>
+      </div>
+      <div :style="{left :state.x + 'px', top : state.y + 'px'}" class="ball"></div>
+      <div ref="bottomMove" :style="{left :state.mx + 'px' , top : state.my + 'px'}"
+           class="bottomMove"></div>
     </div>
-    <div class="ball" :style="{left :x + 'px', top : y + 'px', width : ball +'px', height: ball+'px'}"></div>
-    <div class="bottomMove"
-         :style="{left :mx + 'px' , top : my + 'px',width :moveBottomW +'px',height : moveBottomH+'px'  }"></div>
   </div>
 </template>
 
-<script setup>
-import {onMounted, onUnmounted, reactive, toRefs} from 'vue'
+<script lang="ts" setup>
+import {onMounted, onUnmounted, reactive, ref, watch} from 'vue'
 
-const boxWidth = 500, // 场景宽度
-    boxHeight = 300, // 场景高度
-    ball = 10,//小球的宽高
-    moveBottomH = 5,//移动方块高度
-    moveBottomW = 100//移动方块快读
-
-const strArr = '恭喜你,挑战成功!!'
-
-//用reactive 保存一些可观察信息
+// 绑定移动方块事件
+onMounted(() => {
+  focusIn()
+})
+// 移动出移动方块事件
+onUnmounted(() => {
+  clearInterval(timer)
+})
+// 隐藏的输入框
+const myInput = ref<HTMLInputElement>();
+const focusIn = () => {
+  myInput.value!.focus()
+}
+const noTxt = ref<string>("")
+watch(() => noTxt.value, () => noTxt.value = "")
+// 挡板与小球的初始位置
 const state = reactive({
-  x: boxWidth / 2 - ball / 2,  // 小球x轴位置信息 计算默认位置在中间
-  y: boxHeight - ball - moveBottomH, // 小球Y轴的位置信息 计算默认位置在中间
-  mx: boxWidth / 2 - moveBottomW / 2, //移动方块的位置信息 计算默认位置在中间
-  my: boxHeight - moveBottomH, // 移动方块y轴的的位置信息  计算默认位置在中间
+  x: 238,  // 小球x轴，初始位置：面板宽度/2-小球宽度/2
+  y: 270, // 小球Y轴,初始位置：面板高度-小球高度-挡板高度
+  mx: 200, // 挡板x轴，初始位置：面板宽度/2-挡板宽度/2
+  my: 294, // 挡板y轴，初始位置：面板高度-挡板高度
   // 被打击方块的数组
   arr: Array.from({length: 50}, (_, index) => {
     return {
@@ -40,47 +58,54 @@ const state = reactive({
     }
   }),
   str: '', // 返回挑战成功字眼
-  scroce: 0 // 分数
+  score: 404 // 分数
 })
-// 用toRefs将观察对象的信息解构出来供模板使用
-const {x, y, mx, my, arr, str, scroce} = toRefs(state)
+
 let timer = null, // 小球定时器
     speed = 3,// 小球速度
     map = {x: 10, y: 10},
     timer2 = null, // 挑战成功字眼显示定时器
-    index = 0//挑战成功字眼续个显示的索引值
+    index = 0 // 挑战成功字眼显示的索引值
 
 // 挑战成功字眼续个显示的方法
-const strFun = () => {
-  if (strArr.length === index) clearInterval(timer2)
-  state.str += strArr.substring(index, 1)
-  index++
+const strFun = (strArr: string): void => {
+  timer2 = setInterval(() => {
+    // 如果文字放完了就结束定时器
+    if (index === strArr.length) {
+      clearInterval(timer2)
+      window.location.reload()
+    }
+    // 放文字
+    state.str += strArr.substring(index, index + 1)
+    index++
+  }, 500)
 }
 
 
 //移动小球的方法
-// 1.这里同过变量map 对象来记录坐标信息, 确定小球碰到 左右上 及移动方块是否回弹
-// 2.循环砖块检测小球碰撞到砖块消失
+// 1. 确定小球碰到 左右上 及移动方块是否回弹
+const bottomMove = ref<HTMLDivElement>()
+// 2. 循环砖块检测小球碰撞到砖块消失
+const brickRef = ref<HTMLDivElement[]>([])
 const moveBall = () => {
-  const {offsetTop, offsetHeight, offsetLeft, offsetWidth} = document.querySelector('.bottomMove')
+  const {offsetTop, offsetHeight, offsetLeft, offsetWidth} = bottomMove.value
   if (state.x <= 0) {
     map.x = speed
-  } else if (state.x > boxWidth - ball) {
+  } else if (state.x > 500 - 24) {
     map.x = -speed
   }
   if (state.y <= 0) {
     map.y = speed
   }
-  if (state.y >= offsetTop - offsetHeight &&
+  if (state.y + 19 >= offsetTop - offsetHeight &&
       state.y <= offsetTop + offsetHeight &&
-      state.x >= offsetLeft &&
+      state.x + 24 >= offsetLeft &&
       state.x < offsetLeft + offsetWidth) {
     map.y = -speed
   }
-  if (state.y > boxHeight) {
+  if (state.y > 300) {
     clearInterval(timer)
-    alert('game over')
-    window.location.reload()
+    strFun("游戏结束")
   }
   Array.from(state.arr).forEach((item, index) => {
     const {
@@ -88,38 +113,35 @@ const moveBall = () => {
       offsetTop,
       offsetWidth,
       offsetHeight
-    } = document.querySelectorAll('.kuai')[index]
-    if (state.x > offsetLeft
+    } = brickRef.value[index]
+    if (state.x + 24 > offsetLeft
         && state.x < offsetLeft + offsetWidth
-        && state.y > offsetTop
+        && state.y + 24 > offsetTop
         && state.y < offsetTop + offsetHeight) {
       if (!state.arr[index].active) {
-        state.scroce += 100
+        state.score += 1
       }
       state.arr[index].active = true
     }
   })
+  // 游戏通关
   if (Array.from(state.arr).every(item => item.active)) {
     clearInterval(timer)
-    timer2 = setInterval(strFun, 1000)
+    strFun("挑战成功")
   }
   state.x = state.x += map.x
   state.y = state.y += map.y
 }
 
-//移动方块左右移动方法 ,接住小球
-const bottomMove = ev => {
-  if (ev.code === 'Space') clearInterval(timer)
-  switch (ev.key) {
-    case 'ArrowRight':
-      state.mx += 100
-      break
-    case  'ArrowLeft':
-      state.mx -= 100
-      break
-  }
+// 方块左移
+const moveLeft = () => {
+  state.mx -= 100
   state.mx = state.mx < 0 ? 0 : state.mx
-  state.mx = state.mx > boxWidth - moveBottomW ? boxWidth - moveBottomW : state.mx
+}
+// 方块右移
+const moveRight = () => {
+  state.mx += 100
+  state.mx = state.mx > 400 ? 400 : state.mx
 }
 // 暂停游戏
 const stop = () => {
@@ -129,65 +151,88 @@ const stop = () => {
 const start = () => {
   timer = setInterval(moveBall, 20)
 }
-
-// 绑定移动方块事件
-onMounted(() => {
-  document.addEventListener('keyup', bottomMove)
-})
-// 移动出移动方块事件
-onUnmounted(() => {
-  clearInterval(timer)
-})
 </script>
 
-<style>
-
-.bottomMove {
-  width: 100px;
-  height: 10px;
-  background: red;
-  position: absolute;
-  transition-duration: 100ms;
-  transition-timing-function: ease-out;
-}
-
-.ball {
-  width: 20px;
-  height: 20px;
-  background-color: red;
-  border-radius: 50%;
-  position: absolute;
-}
-
-.kuaiBox {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.kuai {
-  width: 30px;
-  height: 10px;
-  background: red;
-  margin: 10px;
-  transition-duration: 100ms;
-  transition-timing-function: ease-in;
-}
-
-.str {
+<style lang="scss">
+/*整个界面*/
+.panel {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f3d19e;
   text-align: center;
-  font-size: 50px;
-  color: red;
+  /*隐藏的input*/
+  .myInput {
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
 
-}
+  /*游戏面板*/
+  .box {
+    justify-content: center;
+    width: 500px;
+    height: 300px;
+    margin: 0 auto;
+    position: relative;
+    border-radius: 8px;
+    border: 8px solid #b88230;
+    background-color: #CDD0D6;
+    overflow: hidden;
+    /*挑战成功的字眼*/
+    .str {
+      font-family: fangsong;
+      font-size: 50px;
+      -webkit-text-stroke: 2px #F56C6C;
+      color: red;
+    }
 
-.box {
+    /*小球*/
+    .ball {
+      width: 24px;
+      height: 24px;
+      background-image: radial-gradient(circle, red, #c45656, #f89898);
+      border-radius: 50%;
+      position: absolute;
+    }
 
-  justify-content: center;
-  width: 500px;
-  height: 500px;
-  margin: 0 auto;
-  position: relative;
-  border: 5px solid red;
-  overflow: hidden;
+    /*砖块区域*/
+    .brickBox {
+      display: flex;
+      flex-wrap: wrap;
+      /*单个砖块*/
+      .brick {
+        width: 48px;
+        height: 20px;
+        margin: 1px;
+        border-radius: 5px;
+        transition-duration: 100ms;
+        transition-timing-function: ease-in;
+        /*第(除以3余0)个砖块*/
+        &:nth-child(3n+0) {
+          background: linear-gradient(#f89898, #337ecc);
+        }
+
+        /*第(除以3余1)个砖块*/
+        &:nth-child(3n+1) {
+          background: linear-gradient(#95d475, #337ecc);
+        }
+
+        /*第(除以3余2)个砖块*/
+        &:nth-child(3n+2) {
+          background: linear-gradient(#eebe77, #529b2e);
+        }
+      }
+    }
+
+    /*挡板*/
+    .bottomMove {
+      width: 100px;
+      height: 6px;
+      background: #529b2e;
+      position: absolute;
+      transition-duration: 100ms;
+      transition-timing-function: ease-out;
+    }
+  }
 }
 </style>
