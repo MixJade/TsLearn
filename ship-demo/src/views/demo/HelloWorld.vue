1f2be502771f20d39b6d1b5c6ac52a4afa417c07
MixJade <1114185977@qq.com>
2023-08-12 11:35:34 +0800

    流光溢彩的按钮

--- ship-demo/src/views/demo/HelloWorld.vue
+++ ship-demo/src/views/demo/HelloWorld.vue
@@ -1,20 +1,24 @@
-<script lang="ts" setup>
-import {Check, Delete, Edit, Message, Search, Star,} from '@element-plus/icons-vue'</script>
 <template>
-  <el-row class="mb-4">
-    <el-button round type="primary">Primary</el-button>
-    <el-button round type="success">Success</el-button>
-    <el-button round type="info">Info</el-button>
-    <el-button round type="warning">Warning</el-button>
-    <el-button round type="danger">Danger</el-button>
-  </el-row>
-
-  <el-row>
-    <el-button :icon="Search" circle/>
-    <el-button :icon="Edit" circle type="primary"/>
-    <el-button :icon="Check" circle type="success"/>
-    <el-button :icon="Message" circle type="info"/>
-    <el-button :icon="Star" circle type="warning"/>
-    <el-button :icon="Delete" circle type="danger"/>
-  </el-row>
+  <div class="hello">
+    <MyBtn @click="x">你好啊</MyBtn>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import MyBtn from "@/components/MyBtn.vue";
+
+const x = () => {
+  alert("你好")
+}
+</script>
+
+<style lang="scss" scoped>
+.hello {
+  height: 98vh;
+  width: 98vw;
+  text-align: center;
+  background-color: #909399;
+  z-index: 1;
+}
+
+</style>
