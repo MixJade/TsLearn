18add7d54f3da8c1cae049c2652aeca1da5c7e5a
MixJade <1114185977@qq.com>
2023-08-22 17:37:37 +0800

    :fire: rememberMe按钮样式，部分代码精简

--- ship-demo/src/views/demo/HelloWorld.vue
+++ ship-demo/src/views/demo/HelloWorld.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="hello">
     <MyBtn @click="x">你好啊</MyBtn>
-    <div class="frame">
-      <MyInput/>
-    </div>
     <VueFoot/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import MyBtn from "@/components/button/MyBtn.vue";
-import MyInput from "@/components/MyInput.vue";
 import VueFoot from "@/components/VueFoot.vue";
 
 const x = () => {
@@ -27,16 +23,5 @@ const x = () => {
   /* 设置z-index,防止遮挡btn的伪元素 */
   position: absolute;
   z-index: -2;
-  /* 输入框外面的元素 */
-  .frame {
-    background-color: white;
-    border: 2px solid #17a2b8;
-    width: 250px;
-    position: relative;
-    left: 50%;
-    margin-left: -125px;
-    margin-top: 30px;
-    padding: 5px;
-  }
 }
 </style>
