326e95b5dfce23d23f60ed733cc47198ef6ff322
MixJade <1114185977@qq.com>
2024-01-05 14:57:56 +0800

    删除vue的初学代码，因为没有注释

--- vue-project/src/main.ts
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './assets/main.css'
-// 路由
-import router from './router'
-
-const app: App = createApp(App)
-app.use(router)
-app.mount('#app')
