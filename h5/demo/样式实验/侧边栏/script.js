2b973bd8469f1308eb713a0ab8aa7e619b5630c5
MixJade <111669273+MixJade@users.noreply.github.com>
2024-06-01 22:32:40 +0800

    删除无用代码

--- h5/demo/样式实验/侧边栏/script.js
+++
@@ -1,8 +0,0 @@
-const body=document.querySelector('body')
-const sidebar=body.querySelector('nav')
-const toggle=body.querySelector('.toggle')
-
-
-toggle.addEventListener('click',()=>{
-    sidebar.classList.toggle('close')
-})
