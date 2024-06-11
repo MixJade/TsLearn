765bed63151c676d9f4c4993c9e7eb57d2b20a63
MixJade <111669273+MixJade@users.noreply.github.com>
2024-06-11 21:28:57 +0800

    餐盘布局的ts函数优化

--- ts01/ts/askHelp/插入-来自《餐盘布局2》.ts
+++ ts01/ts/askHelp/插入-来自《餐盘布局2》.ts
@@ -1,121 +1,164 @@
 const mealLayout = (() => {
     // 默认链接
     interface defA {
-        type: string;
         href: string;
         name: string;
         remark: string;
     }
 
-    // 链接分组
+    // 链接小组
     interface aGroup {
+        name: string;
+        ul: defA[];
+    }
+
+    // 链接分组
+    interface dataGroup {
         longNav: string;
-        mainBox: defA[];
-        bigSide: defA[];
-        bottom1: defA[];
-        bottom2: defA[];
-        bottom3: defA[];
+        mainBox: aGroup;
+        bigSide: aGroup;
+        bottom1: aGroup;
+        bottom2: aGroup;
+        bottom3: aGroup;
         updateTime: string;
     }
 
     // 初始数据
-    const myData: aGroup[] = [{
+    const myData: dataGroup[] = [{
         longNav: "初始教程",
-        mainBox: [{
-            type: "VUE",
-            href: "https://cn.vuejs.org/guide/quick-start.html",
-            name: "Vue上手",
-            remark: "一切根基"
-        }, {
-            type: "组件",
-            href: "https://element-plus.org/zh-CN/",
-            name: "Element-Plus",
-            remark: "项目组件库"
-        }, {
-            type: "图标",
-            href: "https://element-plus.org/zh-CN/component/icon.html",
-            name: "Element的Icon",
-            remark: "美化图标"
-        },],
-        bigSide: [{
-            type: "样式",
-            href: "https://www.sass.hk/docs/",
-            name: "Sass官网教程",
-            remark: "但不建议看这个"
-        }, {
-            type: "样式",
-            href: "https://www.runoob.com/sass/sass-install.html",
-            name: "Sass菜鸟教程",
-            remark: "这个写的不错"
-        }, {
-            type: "格式",
-            href: "https://zh-hans.eslint.org/docs/latest/use/getting-started",
-            name: "ESLint官网",
-            remark: "格式化工具"
-        }, {
-            type: "样式",
-            href: "https://www.bootstrap.cn/doc/read/113.html",
-            name: "bootstrap官网",
-            remark: "一些样式要从这里来"
-        }, {type: "请求", href: "https://www.axios-http.cn/docs/intro", name: "Axios官网", remark: "前端请求"},],
-        bottom1: [],
-        bottom2: [],
-        bottom3: [],
+        mainBox: {
+            name: "参考网站",
+            ul: [{
+                href: "https://cn.vuejs.org/guide/quick-start.html",
+                name: "Vue上手",
+                remark: "一切根基"
+            }, {
+                href: "https://element-plus.org/zh-CN/",
+                name: "Element-Plus",
+                remark: "项目组件库"
+            }, {
+                href: "https://element-plus.org/zh-CN/component/icon.html",
+                name: "Element的Icon",
+                remark: "美化图标"
+            },]
+        },
+        bigSide: {
+            name: "样式参考",
+            ul: [{
+                href: "https://www.sass.hk/docs/",
+                name: "Sass官网教程",
+                remark: "但不建议看这个"
+            }, {
+                href: "https://www.runoob.com/sass/sass-install.html",
+                name: "Sass菜鸟教程",
+                remark: "这个写的不错"
+            }, {
+                href: "https://www.bootstrap.cn/doc/read/113.html",
+                name: "bootstrap官网",
+                remark: "一些样式要从这里来"
+            },]
+        },
+        bottom1: {
+            name: "框架官网",
+            ul: [{
+                href: "https://www.axios-http.cn/docs/intro", name: "Axios官网", remark: "前端请求"
+            }, {
+                href: "https://zh-hans.eslint.org/docs/latest/use/getting-started",
+                name: "ESLint官网",
+                remark: "格式化工具"
+            },
+            ]
+        },
+        bottom2: {name: "", ul: []},
+        bottom3: {name: "", ul: []},
         updateTime: "2024-6-11 16:32"
     }, {
         longNav: "补充知识",
-        mainBox: [{
-            type: "样式",
-            href: "https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties",
-            name: "CSS自定属性",
-            remark: "方便调配色"
-        }, {
-            type: "样式",
-            href: "https://www.jianshu.com/p/d875f680fc6b",
-            name: "CSS波浪加号大于号",
-            remark: "一个有用的语法"
-        }, {
-            type: "样式",
-            href: "https://zhuanlan.zhihu.com/p/96721026",
-            name: "前端尺寸vw、vh、rem、em",
-            remark: "了解即可"
-        }, {
-            type: "前端知识",
-            href: "https://blog.csdn.net/qq_42231156/article/details/124506627",
-            name: "计算属性传参",
-            remark: ""
-        }, {
-            type: "前端知识",
-            href: "https://blog.csdn.net/GraySakura/article/details/104821218",
-            name: "div换行设置",
-            remark: ""
-        }, {
-            type: "前端知识",
-            href: "https://juejin.cn/post/7074448287352225823",
-            name: "Vue3关于路由动画的注意事项",
-            remark: ""
-        }, {
-            type: "前端知识",
-            href: "https://juejin.cn/post/7137460519497105439",
-            name: "Vue3关于滚动条的细节",
-            remark: "包括flex翻转，用做聊天页面"
-        }, {type: "前端知识", href: "https://juejin.cn/post/7132656370901336101", name: "Vue3的h函数", remark: ""},],
-        bigSide: [],
-        bottom1: [{
-            type: "后端",
-            href: "https://blog.csdn.net/hunt_er/article/details/81427804",
-            name: "无Java环境下运行jar文件",
-            remark: "感觉不如IDE"
-        }],
-        bottom2: [{
-            type: "别人仓库",
-            href: "https://gitee.com/haojunchuan/web_every_day/tree/master/parallax_website",
-            name: "别人的仓库",
-            remark: "我会吸收一些东西"
-        }],
-        bottom3: [],
+        mainBox: {
+            name: "前端知识", ul: [{
+                href: "https://blog.csdn.net/qq_42231156/article/details/124506627",
+                name: "计算属性传参",
+                remark: ""
+            }, {
+                href: "https://blog.csdn.net/GraySakura/article/details/104821218",
+                name: "div换行设置",
+                remark: ""
+            }, {
+                href: "https://juejin.cn/post/7074448287352225823",
+                name: "Vue3关于路由动画的注意事项",
+                remark: ""
+            }, {
+                href: "https://juejin.cn/post/7137460519497105439",
+                name: "Vue3关于滚动条的细节",
+                remark: "包括flex翻转，用做聊天页面"
+            }, {href: "https://juejin.cn/post/7132656370901336101", name: "Vue3的h函数", remark: ""},]
+        },
+        bigSide: {
+            name: "样式知识",
+            ul: [{
+                href: "https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties",
+                name: "CSS自定属性",
+                remark: "方便调配色"
+            }, {
+                href: "https://www.jianshu.com/p/d875f680fc6b",
+                name: "CSS波浪加号大于号",
+                remark: "一个有用的语法"
+            }, {
+                href: "https://zhuanlan.zhihu.com/p/96721026",
+                name: "前端尺寸vw|vh|rem|em",
+                remark: "了解即可"
+            },]
+        },
+        bottom1: {
+            name: "后端",
+            ul: [{
+                href: "https://blog.csdn.net/hunt_er/article/details/81427804",
+                name: "无Java环境下运行jar文件",
+                remark: "感觉不如IDE"
+            }]
+        },
+        bottom2: {
+            name: "别人仓库",
+            ul: [{
+                href: "https://gitee.com/haojunchuan/web_every_day/tree/master/parallax_website",
+                name: "别人的仓库",
+                remark: "我会吸收一些东西"
+            }]
+        },
+        bottom3: {name: "", ul: []},
         updateTime: "2024-6-11 17:52"
-    },]
+    }, {
+        longNav: "小小工具",
+        mainBox: {
+            name: "配色", ul: [
+                {
+                    href: "https://element-plus.gitee.io/zh-CN/component/color.html",
+                    name: "ElementPlus配色",
+                    remark: "一个规范"
+                },
+                {href: "https://www.cnblogs.com/5H5H/p/9784015.html", name: "颜色代码", remark: "各种颜色的英文名称"},
+            ]
+        },
+        bigSide: {
+            name: "样式",
+            ul: [
+                {href: "https://uiverse.io/cards", name: "UI_Verse", remark: "不错的css样式库，但代码有冗余，记得清理"},
+                {
+                    href: "https://cdn.bootcdn.net/ajax/libs/bootstrap/5.2.3/css/bootstrap.css",
+                    name: "BootStrap的css",
+                    remark: "老大哥，直接copy"
+                }
+            ]
+        },
+        bottom1: {
+            name: "工具", ul: [
+                {href: "https://tooltt.com/json2typescript/", name: "Json转Ts接口", remark: "在线网站，JSON键要加双引号"}
+            ]
+        },
+        bottom2: {name: "", ul: []},
+        bottom3: {name: "", ul: []},
+        updateTime: "2024-6-11 21:26",
+    }]
     // 获取dom元素
     const longNavUl = document.getElementById("long-nav-ul");
     const mainBoxUl = document.getElementById("main-box-ul");
@@ -124,6 +167,13 @@ const mealLayout = (() => {
     const bottom2Ul = document.getElementById("bottom2-ul");
     const bottom3Ul = document.getElementById("bottom3-ul");
     const updateTime = document.getElementById("updateTime");
+    // 获取标题
+    const mainBoxH = document.getElementById("main-box-h");
+    const bigSideH = document.getElementById("big-side-h");
+    const bottom1H = document.getElementById("bottom1-h");
+    const bottom2H = document.getElementById("bottom2-h");
+    const bottom3H = document.getElementById("bottom3-h");
+
 
     // 正式初始化
     window.onload = () => {
@@ -163,33 +213,40 @@ const mealLayout = (() => {
         if (mainBoxUl == null || bigSideUl == null
             || bottom1Ul == null || bottom2Ul == null || bottom3Ul == null
             || updateTime == null) return;
-        const aGroup = myData[dataIndex];
+        if (mainBoxH == null || bigSideH == null
+            || bottom1H == null || bottom2H == null || bottom3H == null) return;
+        const datum = myData[dataIndex];
         // 开始设置值
         // 上面两个
         mainBoxUl.innerHTML = '';
-        aGroup.mainBox.forEach(item => mainBoxUl.innerHTML += getUlStr(item))
+        mainBoxH.innerText = datum.mainBox.name
+        datum.mainBox.ul.forEach(item => mainBoxUl.innerHTML += getUlStr(item))
         bigSideUl.innerHTML = '';
-        aGroup.bigSide.forEach(item => bigSideUl.innerHTML += getUlStr2(item))
+        bigSideH.innerText = datum.bigSide.name
+        datum.bigSide.ul.forEach(item => bigSideUl.innerHTML += getUlStr2(item))
         // 下面三个
         bottom1Ul.innerHTML = '';
-        aGroup.bottom1.forEach(item => bottom1Ul.innerHTML += getUlStr2(item))
+        bottom1H.innerText = datum.bottom1.name
+        datum.bottom1.ul.forEach(item => bottom1Ul.innerHTML += getUlStr2(item))
         bottom2Ul.innerHTML = '';
-        aGroup.bottom2.forEach(item => bottom2Ul.innerHTML += getUlStr2(item))
+        bottom2H.innerText = datum.bottom2.name
+        datum.bottom2.ul.forEach(item => bottom2Ul.innerHTML += getUlStr2(item))
         bottom3Ul.innerHTML = '';
-        aGroup.bottom3.forEach(item => bottom3Ul.innerHTML += getUlStr2(item))
+        bottom3H.innerText = datum.bottom3.name
+        datum.bottom3.ul.forEach(item => bottom3Ul.innerHTML += getUlStr2(item))
         // 更新时间
-        updateTime.innerText = aGroup.updateTime
+        updateTime.innerText = datum.updateTime
     }
 
     const getUlStr = (defA: defA): string => {
-        return `<li>${defA.type}:<a href="${defA.href}">【${defA.name}】</a>${defA.remark}</li>`
+        return `<li><a href="${defA.href}">【${defA.name}】</a>${defA.remark}</li>`
     }
 
     const getUlStr2 = (defA: defA): string => {
         if (defA.remark === "") {
-            return `<li>${defA.type}:<a href="${defA.href}">【${defA.name}】</a></li>`
+            return `<li><a href="${defA.href}">【${defA.name}】</a></li>`
         } else {
-            return `<li>${defA.type}:<a href="${defA.href}" class="tooltip">【${defA.name}】<span class="tip-text">${defA.remark}</span></a></li>`
+            return `<li><a href="${defA.href}" class="tooltip">【${defA.name}】<span class="tip-text">${defA.remark}</span></a></li>`
         }
     }
 
