1570e08c15d51e1aa9e9be99302ae0a568ff71ac
MixJade <111669273+MixJade@users.noreply.github.com>
2025-03-26 19:59:56 +0800

    Revert "父子组件传参"
    This reverts commit fc9aff78fcd5877e0f29bcc056ab86c872b32d0a.

--- payment-ui/src/App.vue
+++ payment-ui/src/App.vue
@@ -1,21 +1,6 @@
 <template>
-  <Toast ref="childRef"/>
   <router-view></router-view>
 </template>
-<script lang="ts" setup>
-import Toast from "@/components/pop/Toast.vue";
-import {provide, ref} from "vue";
-
-// 引用子组件
-const childRef = ref(null);
-// 调用子组件暴露的方法
-const countSymbol = (ss: number) => {
-  if (childRef.value) {
-    childRef.value.increment(ss);
-  }
-};
-
-provide("countSymbol", countSymbol);
-</script>
+<script lang="ts" setup></script>
 <style scoped>
 </style>
--- payment-ui/src/views/calendar.vue
+++ payment-ui/src/views/calendar.vue
@@ -1,12 +1,11 @@
 <template>
   <h1>{{ selectYear }}-{{ selectMonth }}</h1>
-  <button type="button" @click="callChildMethod">调用父组件</button>
   <YearCalendar :year="selectYear" :month="selectMonth" @upYear="upYear" @upMonth="upMonth"/>
 </template>
 
 <script lang="ts" setup>
 import YearCalendar from "@/components/show/YearCalendar.vue";
-import {inject, ref} from "vue";
+import {ref} from "vue";
 
 // 获取当前年份
 const selectYear = ref<number>(new Date().getFullYear());
@@ -18,15 +17,6 @@ const upYear = (newYear: number): void => {
 const upMonth = (newMonth: number): void => {
   selectMonth.value = newMonth;
 }
-
-// 注入父组件提供的函数
-const countSymbol = inject('countSymbol');
-const callChildMethod = () => {
-  // 使用注入的函数
-  if (countSymbol) {
-    countSymbol("123");
-  }
-};
 </script>
 
 <style lang="sass" scoped>
