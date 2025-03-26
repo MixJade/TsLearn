fc9aff78fcd5877e0f29bcc056ab86c872b32d0a
MixJade <1114185977@qq.com>
2025-03-26 18:01:27 +0800

    父子组件传参

--- payment-ui/src/App.vue
+++ payment-ui/src/App.vue
@@ -1,6 +1,21 @@
 <template>
+  <Toast ref="childRef"/>
   <router-view></router-view>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Toast from "@/components/pop/Toast.vue";
+import {provide, ref} from "vue";
+
+// 引用子组件
+const childRef = ref(null);
+// 调用子组件暴露的方法
+const countSymbol = (ss: number) => {
+  if (childRef.value) {
+    childRef.value.increment(ss);
+  }
+};
+
+provide("countSymbol", countSymbol);
+</script>
 <style scoped>
 </style>
