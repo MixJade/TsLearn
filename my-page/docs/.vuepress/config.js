7e8c3cc6924bca51d3d4dee051d43edd2562fdd9
MixJade <1114185977@qq.com>
2026-03-29 23:29:54 +0800

    vuePress搜索

--- my-page/docs/.vuepress/config.js
+++ my-page/docs/.vuepress/config.js
@@ -1,7 +1,11 @@
 import {defaultTheme} from '@vuepress/theme-default'
 import {defineUserConfig} from 'vuepress'
 import {viteBundler} from '@vuepress/bundler-vite'
+import {searchPlugin} from '@vuepress/plugin-search'
 
+// 安全导入JSON
+const myNavbar = require('./myNavbar.json')
+const mySidebar = require('./mySidebar.json')
 export default defineUserConfig({
     lang: 'zh-CN',
     title: 'MixJade',
@@ -11,10 +15,26 @@ export default defineUserConfig({
         logo: "/my-logo.png",
         logoDark: "/black-logo.jpg",
         // 导航栏配置
-        navbar: require('./myNavbar.json'),
+        navbar: myNavbar,
         // 侧边栏配置
-        sidebar: require('./mySidebar.json'),
+        sidebar: mySidebar,
     }),
-
-    bundler: viteBundler(),
+    plugins: [
+        searchPlugin({
+            maxSuggestions: 10,
+            hotKeys: ['ctrl+k'],
+        }),
+    ],
+    bundler: viteBundler({
+        viteOptions: {
+            css: {
+                preprocessorOptions: {
+                    scss: {
+                        // 关闭 Dart Sass 弃用警告
+                        silenceDeprecations: ['legacy-js-api'],
+                    },
+                },
+            },
+        },
+    }),
 })
